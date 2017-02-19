77d27de9c68f79b43257190f560fbb04fac02cfa
ZeeLog <Danjel.Zammit@gmail.com>
2017-02-19 01:00:46 +0100

    switch to common mini phone config
    Change-Id: I8d0acb57d0fb0858add27c3520b64286f640f373

--- lineage.mk
+++ lineage.mk
@@ -28,7 +28,7 @@ $(call inherit-product, device/samsung/tuna/overrides.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
